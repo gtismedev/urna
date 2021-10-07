41c83d088e88dc286bb01d0d59a8de54553af610
Arthur <49170441+arthurbttf@users.noreply.github.com>
2021-10-07 16:49:31 -0300

    testes no JS

--- main.js
+++ main.js
@@ -3,20 +3,23 @@ var teclafx = new Audio('sons/tecla.mp3');//Som ao teclar.
 var numero = '', count = 0, troca_img, csv, hiddenElement,contadorVotos = 0, somaColuna='';
 var hora, minuto, segundo;
 let data = [];
-//data[0][0] = '01'
-console.log(data)
+var votosC1, votosC2, votosC3, votosC4, votosC5, votosC6, votosNulo = 0;
+//console.log(data)
+
 
 function confirma()
 {
 	//console.log(numero);
-	//confirmasfx.play();
-	//window.location = 'final.html';
 
 	console.log(tempo());
+	if(numero != '01' && numero != '02' && numero != '03' && numero != '04' && numero != '05' && numero != '06')
+		votosNulo++;
 	data[contadorVotos] = [numero,'1',tempo()]
 	contadorVotos++;
 	somaColuna = data.length;
-	console.log(data)
+	/*setTimeout(function(){
+				window.location.href = 'final.html';
+			}, 1000);*/
 }
 
 //botão corrige, para apagar os numeros digitados
@@ -24,7 +27,7 @@ function corrige()
 {
 	teclafx.play();
 	//se mandar a variavel sem nada, aparece a imagem do voto nulo, por isso esse if
-	if(numero != '' && count == 2)
+	if(numero != '' && count == 3)
 		showHide(numero);
 	numero = '';
 	count = 0;
@@ -51,12 +54,19 @@ function botao(clicked_id)
 	teclafx.play();
 }
 
+
+
 //fazer download dos resultados pra excel
 function download_csv()
 {
-	somaColuna++;
-	data[contadorVotos+1] = ['',"=soma(B2:B"+somaColuna+")",'']
-    csv = 'sep=,\nChapa,Votos,Hora\n';
+	//ainda falta contar os votos brancos
+	votosC1 = '=SOMASE(A12:A300;"01";B12:B300)';
+	votosC2 = '=SOMASE(A12:A300;"02";B12:B300)';
+	votosC3 = '=SOMASE(A12:A300;"03";B12:B300)';
+	votosC4 = '=SOMASE(A12:A300;"04";B12:B300)';
+	votosC5 = '=SOMASE(A12:A300;"05";B12:B300)';
+	votosC6 = '=SOMASE(A12:A300;"06";B12:B300)';
+	csv = 'sep=,\nChapa,Quantidade de Votos\n01,'+votosC1+'\n02,'+votosC2+'\n03,'+votosC3+'\n04,'+votosC4+'\n05,'+votosC5+'\n06,'+votosC6+'\nNulos,'+votosNulo+'\nBrancos,\n\nChapa,Votos,Hora\n';
     data.forEach(function(row) {
 	    csv += row.join(',');
 	    csv += "\n";
