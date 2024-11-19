08b8c0cbe56554d2f754f2689860fb28ea6d0f1f
FellipeMiguel <fellipemiguel1@icloud.com>
2024-11-18 23:43:03 -0300

    feat: tipo de votot

--- scripts/main.js
+++ scripts/main.js
@@ -29,26 +29,20 @@ let voteTotalC1 = 0,
   voteTotalC2 = 0,
   voteTotalC3 = 0,
   voteTotalC4 = 0,
-  voteTotalC5 = 0,
-  voteTeacher = 0,
+  voteTotalC5 = 0;
+let voteTeacher = 0,
   voteEmployee = 0,
   voteStudent = 0,
   voteParent = 0;
 
-preenche_lista();
-
-function iniciar() {
-  location.replace("inicio.html");
-}
-
 function carregaDados() {
-  elementoPai = document.getElementById("tabela_corpo");
+  var elementoPai = document.getElementById("tabela_corpo");
 
-  for (i = 0; i < listaVoto.length; i++) {
-    tr = document.createElement("tr");
-    td1 = document.createElement("td");
-    td2 = document.createElement("td");
-    td3 = document.createElement("td");
+  for (var i = 0; i < listaVoto.length; i++) {
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
 
     td1.textContent = listaVoto[i].nome;
     td2.textContent = listaVoto[i].numero;
@@ -62,9 +56,55 @@ function carregaDados() {
   }
 }
 
-//registra o voto digitado
+document.addEventListener("DOMContentLoaded", function () {
+  var selectElement = document.getElementById("type-vote");
+
+  window.getSelectedValue = function () {
+    var selectedValue = selectElement.value;
+    console.log("Tipo de voto selecionado:", selectedValue);
+    return selectedValue;
+  };
+
+  getSelectedValue();
+
+  selectElement.addEventListener("change", getSelectedValue);
+
+  preenche_lista();
+});
+
+function iniciar() {
+  location.replace("inicio.html");
+}
+
+function zeresima() {
+  nome = [
+    "Nulo",
+    "Branco",
+    "Chapa 1",
+    "Chapa 2",
+    "Chapa 3",
+    "Chapa 4",
+    "Chapa 5",
+  ];
+  numero = ["nulo", "BR", "01", "02", "03", "04", "05"];
+  for (i = 0; i < nome.length; i++) {
+    localStorage.setItem(
+      nome[i],
+      JSON.stringify({ nome: nome[i], numero: numero[i], votos: 0 })
+    );
+  }
+  let userPassword = prompt("Digite a senha");
+
+  if (userPassword === password) {
+    location.replace("resultados.html");
+  } else {
+    window.alert("Senha incorreta");
+  }
+}
+
 function confirma() {
   if (numero != "") {
+    let tipoVoto = getSelectedValue();
     if (
       numero != "01" &&
       numero != "02" &&
@@ -72,25 +112,33 @@ function confirma() {
       numero != "04" &&
       numero != "05" &&
       numero != "BR"
-    )
+    ) {
       numero = "nulo";
-
+    }
     botaoConfirmar = document.getElementById("buttonON");
     botaoConfirmarOff = document.getElementById("buttonOFF");
     botaoConfirmar.style.display = "none";
     botaoConfirmarOff.style.display = "initial";
-
     confirmasfx.play();
-    //serve para adicionar um voto ao número digitado
     for (i = 0; i < listaVoto.length; i++) {
-      if (listaVoto[i].numero == numero)
+      if (listaVoto[i].numero == numero) {
         listaVoto[i].votos = parseInt(listaVoto[i].votos + 1);
+        if (!listaVoto[i].tipos) {
+          listaVoto[i].tipos = {
+            teacher: 0,
+            employee: 0,
+            student: 0,
+            parents: 0,
+          };
+        }
+        listaVoto[i].tipos[tipoVoto] += 1; // Atualiza o tipo de voto
+      }
     }
-
+    console.log(
+      `Voto computado: Tipo de voto - ${tipoVoto}, Chapa - ${numero}`
+    );
     atualizarLocalStorage();
-
     if (numero != "" && count == 3) showHide(numero);
-
     numero = "";
     count = 0;
     document.getElementById("tela_numero").innerHTML = "‎ ";
@@ -139,9 +187,9 @@ function showHide(my_id) {
     my_id != "04" &&
     my_id != "05" &&
     my_id != "BR"
-  )
+  ) {
     my_id = "nulo";
-
+  }
   troca_img = document.getElementById(my_id);
   sem_nada = document.getElementById("nada");
   if (troca_img.style.display == "block") {
@@ -155,7 +203,6 @@ function showHide(my_id) {
 
 function end() {
   let userPassword = prompt("Digite a senha");
-
   if (userPassword === password) {
     var para = new URLSearchParams();
     para.append("iniciado", true);
@@ -185,29 +232,39 @@ function cadastrarCandidato() {
   location.replace("inicio.html");
 }
 
-function zeresima() {
-  nome = [
-    "Nulo",
-    "Branco",
-    "Chapa 1",
-    "Chapa 2",
-    "Chapa 3",
-    "Chapa 4",
-    "Chapa 5",
-  ];
-  numero = ["nulo", "BR", "01", "02", "03", "04", "05"];
-  for (i = 0; i < nome.length; i++) {
-    localStorage.setItem(
-      nome[i],
-      JSON.stringify({ nome: nome[i], numero: numero[i], votos: 0 })
-    );
+function preenche_lista() {
+  ls_keys = Object.keys(localStorage);
+  for (i in ls_keys) {
+    listaVoto.push(JSON.parse(localStorage.getItem(ls_keys[i])));
   }
-  let userPassword = prompt("Digite a senha");
 
-  if (userPassword === password) {
-    location.replace("resultados.html");
-  } else {
-    window.alert("Senha incorreta");
+  // Organiza a lista de votos
+  var i = 5;
+  while (cont <= 3) {
+    listaVotoNome = listaVoto[cont].nome;
+    listaVotoNumero = listaVoto[cont].numero;
+    listaVotoVotos = listaVoto[cont].votos;
+    listaVoto[cont].nome = listaVoto[i].nome;
+    listaVoto[cont].numero = listaVoto[i].numero;
+    listaVoto[cont].votos = listaVoto[i].votos;
+    listaVoto[i].nome = listaVotoNome;
+    listaVoto[i].numero = listaVotoNumero;
+    listaVoto[i].votos = listaVotoVotos;
+    if (cont != 2) {
+      cont++;
+      i--;
+    } else if (cont == 2) {
+      cont++;
+      i += 2;
+    }
+  }
+
+  return listaVoto;
+}
+
+function atualizarLocalStorage() {
+  for (i = 0; i < listaVoto.length; i++) {
+    localStorage.setItem(listaVoto[i].nome, JSON.stringify(listaVoto[i]));
   }
 }
 
@@ -244,79 +301,35 @@ function convertePDF(x) {
     pdf.text(182, 650, "COMISSÃO ELEITORAL ESCOLAR");
   }
 
-  source = $("#div_tabela")[0];
+  let source = document.getElementById("div_tabela");
 
-  specialElementHandlers = {
-    // element with id of "bypass" - jQuery style selector
+  let specialElementHandlers = {
     "#bypassme": function (element, renderer) {
-      // true = "handled elsewhere, bypass text extraction"
       return true;
     },
   };
-  margins = {
+
+  let margins = {
     top: 250,
     bottom: 30,
     left: 78,
     width: 522,
   };
+
   pdf.fromHTML(
     source, // HTML string or DOM elem ref.
     margins.left, // x coord
     margins.top,
     {
-      // y coord
       width: margins.width, // max width of content on PDF
       elementHandlers: specialElementHandlers,
     },
-
     function (dispose) {
       pdf.save("Resultados.pdf");
     },
     margins
   );
   alert("Sua votação foi baixada!");
-}
-
-//retorna uma lista com todos os itens guardados no localstorage
-function preenche_lista() {
-  ls_keys = Object.keys(localStorage);
-
-  for (i in ls_keys)
-    listaVoto.push(JSON.parse(localStorage.getItem(ls_keys[i])));
-
-  //sort listaVoto
-  var i = 5;
-  while (cont <= 3) {
-    listaVotoNome = listaVoto[cont].nome;
-    listaVotoNumero = listaVoto[cont].numero;
-    listaVotoVotos = listaVoto[cont].votos;
-    listaVoto[cont].nome = listaVoto[i].nome;
-    listaVoto[cont].numero = listaVoto[i].numero;
-    listaVoto[cont].votos = listaVoto[i].votos;
-    listaVoto[i].nome = listaVotoNome;
-    listaVoto[i].numero = listaVotoNumero;
-    listaVoto[i].votos = listaVotoVotos;
-    if (cont != 2) {
-      cont++;
-      i--;
-    } else if (cont == 2) {
-      cont++;
-      i += 2;
-    }
-    /*cont=0,i=5;
-	cont=1,i=4;
-	cont=2,i=3;
-	cont=3,i=5*/
-  }
-
-  return listaVoto;
-}
-
-//atualiza os novos itens no localstorage
-function atualizarLocalStorage() {
-  for (i = 0; i < listaVoto.length; i++) {
-    localStorage.setItem(listaVoto[i].nome, JSON.stringify(listaVoto[i]));
-  }
 }
 
 function getTempo() {
@@ -355,14 +368,3 @@ function blockButton() {
     buttonContinue.style.display = "none";
   }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var selectElement = document.getElementById("type-vote");
-  function getSelectedValue() {
-    var selectedValue = selectElement.value;
-    console.log("voto:", selectedValue);
-  }
-  getSelectedValue();
-  selectElement.addEventListener("change", getSelectedValue);
-});
-//V(x) = ((P(x) + A(x)) * 50)/(P(x) + A(x)) + ((Prof(x) + F(x)) * 50)/(Prof(x) + F(x))
